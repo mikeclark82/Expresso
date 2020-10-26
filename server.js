3fdea0c1af7ee68a5cf588a0814e3d13564517b1
Mike Clark <mikeclark82@hotmail.com>
2020-10-26 21:09:26 +0000

    Added server.js and migration.js

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-const app = require('express');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const errorHandler = require('errorhandler')
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(errorHandler());
+app.use(bodyParser.json());
+
+app.listen(PORT,()=>{
+    console.log(`App listening on ${PORT}`);
+});
